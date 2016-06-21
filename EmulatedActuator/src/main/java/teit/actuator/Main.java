f58400fde0e73121402dc08aa1c16dc7bfd51b16
Le Duc Hung <d.le@dsg.tuwien.ac.at>
2016-06-21 10:03:50 +0530

    fix bug json

--- EmulatedActuator/src/main/java/teit/actuator/Main.java
+++ EmulatedActuator/src/main/java/teit/actuator/Main.java
@@ -5,7 +5,6 @@
  */
 package teit.actuator;
 
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.IOException;
@@ -19,66 +18,62 @@ import teit.actuator.model.EnumState;
  * @author hungld
  */
 public class Main {
+
     static private EnumState enumState = new EnumState();
     static private EnumControl enumControl = new EnumControl();
     static private String currentState;
     static private List<String> stateList;
     static private List<EnumControl> controlList;
-      public static void main(String[] args) throws  IOException{
-            String jsonString;
-          //convert json string to object java
-          if (args.length > 0){
-              enumState = getJsonString(args[0]);
-          }
-          else{
-                jsonString = SamplesGenerator.generateSwitchDesciption();
-                ObjectMapper mapper = new ObjectMapper();
-                EnumState enumState = mapper.readValue(jsonString, EnumState.class);
-          }
-        
-          	
-                stateList = getState(enumState);
-                controlList = getControl(enumState);
-                currentState = stateList.get(0);
-                System.out.println("CURRENT State: " + currentState);
-                System.out.println("State LIST: " +stateList );
-               
-                enumControl = invoke("turn-off",enumState,null);
-                currentState =getCurrentState(enumControl);
-                System.out.println("Current State UPDATED:" + currentState);
-      }
-    static public List<String> getState(EnumState eState){
-        
+
+    public static void main(String[] args) throws IOException {
+        String jsonString;
+        //convert json string to object java
+        if (args.length > 0) {
+            ObjectMapper mapper = new ObjectMapper();
+            enumState = mapper.readValue(new File(args[0]), EnumState.class);
+        } else {
+            jsonString = SamplesGenerator.generateSwitchDesciption();
+            ObjectMapper mapper = new ObjectMapper();
+            EnumState enumState = mapper.readValue(jsonString, EnumState.class);
+        }
+
+        stateList = getState(enumState);
+        controlList = getControl(enumState);
+        currentState = stateList.get(0);
+        System.out.println("CURRENT State: " + currentState);
+        System.out.println("State LIST: " + stateList);
+
+        enumControl = invoke("turn-off", enumState, null);
+        currentState = getCurrentState(enumControl);
+        System.out.println("Current State UPDATED:" + currentState);
+    }
+
+    static public List<String> getState(EnumState eState) {
+
         return eState.getStateList();
     }
-    static public List<EnumControl> getControl(EnumState eState){
-       return eState.getControlList(); 
+
+    static public List<EnumControl> getControl(EnumState eState) {
+        return eState.getControlList();
     }
-    static public String getCurrentState(EnumControl aControl){
+
+    static public String getCurrentState(EnumControl aControl) {
         return aControl.getEndState();
     }
-    public static EnumControl invoke(String actionName,EnumState eState, String[] parameter){
-        List<EnumControl> controlList= new ArrayList<>();
-        EnumControl control= new EnumControl();
+
+    public static EnumControl invoke(String actionName, EnumState eState, String[] parameter) {
+        List<EnumControl> controlList = new ArrayList<>();
+        EnumControl control = new EnumControl();
         controlList = eState.getControlList();
-        for (EnumControl aControl: controlList){
-            if (aControl.getName().equalsIgnoreCase(actionName)){
-                  
-                control = aControl; 
+        for (EnumControl aControl : controlList) {
+            if (aControl.getName().equalsIgnoreCase(actionName)) {
+
+                control = aControl;
             }
-           
-        }    
-        return control; 
-    }
-    static public EnumState getJsonString(String filePath) throws IOException{
-        
-        EnumState eState = new EnumState();
-        ObjectMapper mapper = new ObjectMapper();
-       
 
-        //Object to JSON in file
-         mapper.writeValue(new File(filePath), eState);
-
-        return eState;
+        }
+        return control;
     }
+
+ 
 }
